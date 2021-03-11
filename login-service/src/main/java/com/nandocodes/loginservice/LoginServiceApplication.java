791ac1a6d928e4a717147c2693f7fa2e990d8931
Fernando Samano <SamanoF21@gmail.com>
2021-03-10 23:43:06 -0800

    Added login-service

---
+++ login-service/src/main/java/com/nandocodes/loginservice/LoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nandocodes.loginservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginServiceApplication.class, args);
+	}
+
+}
