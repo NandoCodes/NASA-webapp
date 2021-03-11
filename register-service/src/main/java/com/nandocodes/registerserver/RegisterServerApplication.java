33d591c28e67047ed60bddc97ae44507028debe5
Fernando Samano <SamanoF21@gmail.com>
2021-03-10 23:39:43 -0800

    Adding register-service

---
+++ register-service/src/main/java/com/nandocodes/registerserver/RegisterServerApplication.java
@@ -0,0 +1,13 @@
+package com.nandocodes.registerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterServerApplication.class, args);
+	}
+
+}
