889278ad8e7e0aa18237ff349b7cd52d2fcf7cdf
Fernando Samano <SamanoF21@gmail.com>
2021-03-11 16:08:20 -0800

    Initialized react  front-end

---
+++ register-service/src/main/java/com/nandocodes/registerserver/controller/RegisterController.java
@@ -0,0 +1,12 @@
+package com.nandocodes.registerserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterController {
+    @GetMapping("nasa/register")
+    public void getRegisterPage() {
+
+    }
+}
