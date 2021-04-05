13a9383beac3096d7012fd0b64b393ddebb34859
NandoCodes <SamanoF21@gmail.com>
2021-04-05 13:50:42 -0700

    Fix Lombok implementation

--- login-service/src/main/java/com/nandocodes/loginservice/models/Role.java
+++ login-service/src/main/java/com/nandocodes/loginservice/models/Role.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name="roles")
 public class Role {
     @Id
@@ -17,23 +20,4 @@ public class Role {
     @Column(length = 20)
     private ERole name;
 
-    public Role() {
-
-    }
-
-    public Role(ERole name) {
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public ERole getName() {
-        return name;
-    }
 }
